b45b73188e72c63a6c180e6a7ece028931afd9e6
warflash <contact@nils.solutions>
2024-04-12 22:03:43 +0200

    init

---
+++ app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+const page = ref(1);
+
+// cached data has no typing/any
+const { data: iLooseTyping } = await useAsyncData(
+  'posts',
+  async () => "hello",
+  {
+    watch: [page],
+    getCachedData(key, nuxtApp) {
+			return nuxtApp.payload.data[key] || nuxtApp.static.data[key] 
+		},
+  }
+);
+
+// No cached data has typing
+const { data: iAmTyped } = await useAsyncData(
+  'posts',
+  async () => "hello",
+  {
+    watch: [page],
+  }
+);
+</script>
